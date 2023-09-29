ed3713165a34c5a041e8ddc9f53cbfbc27e65e3d
ancoka <imancoka@gmail.com>
2023-09-29 16:22:00 +0800

    style(main): 增加banner output

--- main.py
+++ main.py
@@ -11,4 +11,14 @@ def main():
 
 
 if __name__ == '__main__':
+    banner = """
+        ooooo   ooooo                           .oooooo..o                     oooo    oooo  o8o  oooo  oooo  
+        `888'   `888'                          d8P'    `Y8                     `888   .8P'   `"'  `888  `888  
+         888     888  oooo oooo    ooo         Y88bo.       .ooooo.   .ooooo.   888  d8'    oooo   888   888  
+         888ooooo888   `88. `88.  .8'           `"Y8888o.  d88' `88b d88' `"Y8  88888[      `888   888   888  
+         888     888    `88..]88..8'   8888888      `"Y88b 888ooo888 888        888`88b.     888   888   888  
+         888     888     `888'`888'            oo     .d8P 888    .o 888   .o8  888  `88b.   888   888   888  
+        o888o   o888o     `8'  `8'             8""88888P'  `Y8bod8P' `Y8bod8P' o888o  o888o o888o o888o o888o                                                                                                                                                                             
+    """
+    print(banner)
     main()

662e6dae5e7007f83fe2430cd0d1db86e55f48cd
liule <lele.liu@hotmail.com>
2024-12-29 18:32:46 +0800

    修改定时脚本输出

--- wait_to_run.sh
+++ wait_to_run.sh
@@ -11,7 +11,7 @@ while true; do
     # 如果当前时间等于目标时间，运行命令并退出循环
      echo "$CURRENT_TIME | checking ..."
     if [[ "$CURRENT_TIME" == "$TARGET_TIME" ]]; then
-        echo "$CURRENT_TIME | 执行 python main.py..."
+        echo "$CURRENT_TIME | run python main.py..."
         python main.py
         break
     fi
